7433032064c4bf03af2d8c542f95052db1895108
cnyan <canyanmail@163.com>
2020-07-27 10:24:45 +0800

    update

--- action_recog_6axis/re_cnn/re_cnn_train_action.py
+++ action_recog_6axis/re_cnn/re_cnn_train_action.py
@@ -61,7 +61,7 @@ class Cnn_train():
         num_epochs = 500
         criterion = nn.CrossEntropyLoss()  # criterion:惩罚规则-- 损失函数
         optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9, weight_decay=0.01)
-        exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+        exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer_ft, step_size=100, gamma=0.1)
 
         best_model_wts = self.model.state_dict()
         best_acc = 0.0
