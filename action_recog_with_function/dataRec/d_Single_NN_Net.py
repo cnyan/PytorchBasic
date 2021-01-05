e61d34472b2adb2f304622ea0ffa6dfd7d6ba41c
cnyan <canyanmail@163.com>
2021-01-05 18:43:08 +0800

    update

--- action_recog_with_function/dataRec/d_Single_NN_Net.py
+++ action_recog_with_function/dataRec/d_Single_NN_Net.py
@@ -93,14 +93,14 @@ class MyLstmNet(nn.Module):
 
         self.lstm = nn.LSTM(  # LSTM 效果要比 nn.RNN() 好多了
             input_size=7 * axis,  # 图片每行的数据像素点
-            hidden_size=7 * axis*2,  # rnn hidden unit
+            hidden_size=7 * axis * 2,  # rnn hidden unit
             num_layers=1,  # 有几层 RNN layers
             # dropout=0.5,
             batch_first=True,  # input & output 会是以 batch size 为第一维度的特征集 e.g. (batch, time_step, input_size)
-            bidirectional=False,  # 单向LSTM
+            bidirectional=True,  # 单向LSTM
         )
 
-        self.out = nn.Linear(7 * axis*2, 5)  # 输出层
+        self.out = nn.Linear(7 * axis * 2 * 2, 5)  # 输出层
 
     def forward(self, x):
         # x shape (batch, time_step, input_size)
@@ -112,5 +112,9 @@ class MyLstmNet(nn.Module):
         # r_out = F.relu(r_out[:, -1, :])
         # 选取最后一个时间点的 r_out 输出
         # 这里 r_out[:, -1, :] 的值也是 h_n 的值
-        out = self.out(r_out[:, -1, :])
+
+        res = r_out[:, 0, :] + r_out[:, -1, :]
+        out = self.out(res)
+        # output_last = output[:,0,:] # 双向LSTM 获取反向的最后一个output
+        # output_last = output[:,-1,:] # 双向LSTM 获取正向的最后一个output
         return out
