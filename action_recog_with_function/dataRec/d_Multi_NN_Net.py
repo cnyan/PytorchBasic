ead15fd125530c1a90fab4958356f42a018e21d4
cnyan <canyanmail@163.com>
2021-01-06 12:21:33 +0800

    update

--- action_recog_with_function/dataRec/d_Multi_NN_Net.py
+++ action_recog_with_function/dataRec/d_Multi_NN_Net.py
@@ -63,11 +63,13 @@ class MyMultiConvNet(nn.Module):
 class MyMultiResCnnNet(nn.Module):
     def __init__(self, axis):
         super(MyMultiResCnnNet, self).__init__()
+
         self.res1_layer = nn.Sequential(
-            nn.Conv1d(7 * axis, 128, 5, 1, 2),
-            nn.BatchNorm1d(128,eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+            nn.Conv1d(7 * axis, 128, 1, 1, 0),
+            nn.BatchNorm1d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
             nn.ReLU()
         )
+
         self.res2_layer = nn.Sequential(
             nn.Conv1d(128, 128, 1, 1, 0),
             nn.BatchNorm1d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
@@ -79,12 +81,13 @@ class MyMultiResCnnNet(nn.Module):
             nn.BatchNorm1d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
             nn.ReLU(),
         )
+
         self.res3_layer = nn.Sequential(
-            nn.Conv1d(128, 256, 3, 1, 1),
-            nn.Dropout(),
-            nn.ReLU(),
-            nn.MaxPool1d(2, 2)
+            nn.Conv1d(128, 256, 1, 2, 0),
+            nn.BatchNorm1d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+            nn.ReLU()
         )
+
         self.res4_layer = nn.Sequential(
             nn.Conv1d(256, 256, 1, 1, 0),
             nn.BatchNorm1d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
@@ -96,27 +99,28 @@ class MyMultiResCnnNet(nn.Module):
             nn.BatchNorm1d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
             nn.ReLU(),
         )
+
         self.res5_layer = nn.Sequential(
-            nn.Conv1d(256, 256, 3, 1, 1),
-            nn.Dropout(),
+            nn.Conv1d(256, 256, 1, 1, 0),
+            nn.BatchNorm1d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
             nn.ReLU(),
-            nn.MaxPool1d(2, 2)
+            nn.AvgPool1d(2, 2)
         )
         self.classifier = nn.Sequential(
-            nn.Linear(256 * 9, 5)
+            nn.Linear(256 * (36 // 2 // 2), 5)
         )
 
     def forward(self, x):
-        x = self.res1_layer(x)
-        res = x
-        x = self.res2_layer(x)
-        x = x + res
-        x = self.res3_layer(x)
-        res = x
-        x = self.res4_layer(x)
-        x = x + res
-        x = self.res5_layer(x)
-
-        out = x.view(x.size(0), -1)
+        out = self.res1_layer(x)
+        res = out
+        out = self.res2_layer(out)
+        out = out + res
+        out = self.res3_layer(out)
+        res = out
+        out = self.res4_layer(out)
+        out = out + res
+        out = self.res5_layer(out)
+        # print(out.shape)
+        out = out.view(out.size(0), -1)
         output = self.classifier(out)
         return output
